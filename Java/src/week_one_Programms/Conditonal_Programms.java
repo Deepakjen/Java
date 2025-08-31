fc7d817bdc754f6b195052f4667ccb4aa5bd26ac
Deepakjen <112792076+Deepakjen@users.noreply.github.com>
2025-08-31 23:28:34 +0530

    new file commit

---
+++ Java/src/week_one_Programms/Conditonal_Programms.java
@@ -0,0 +1,10 @@
+package week_one_Programms;
+
+public class Conditonal_Programms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Java/src/week_one_Programms/Loops_Questions.java
@@ -0,0 +1,10 @@
+package week_one_Programms;
+
+public class Loops_Questions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
