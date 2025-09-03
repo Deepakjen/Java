63828f9ae50c509cd67404586ca14744d7160ecb
Deepakjen <112792076+Deepakjen@users.noreply.github.com>
2025-09-03 07:00:37 +0530

    oops commit

---
+++ Java/src/week_2/EmpMain.java
@@ -0,0 +1,26 @@
+package week_2;
+
+public class EmpMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Class_Object cb = new Class_Object(101, "Deepak", 'A');
+		/*
+		 * By Using object reference variable.
+		 * cb.Eid = 101; cb.Ename = "Deepak"; cb.grd = 'A'; cb.printData();
+		 */
+		
+		/* cb.setData(101, "Deepak", 'A'); */
+		
+		
+		
+		
+		cb.printData();
+		
+		
+		
+
+	}
+
+}
