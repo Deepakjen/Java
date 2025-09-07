ae3eb265f2c3c20d38b1dded4e64532af6c03111
Deepakjen <112792076+Deepakjen@users.noreply.github.com>
2025-09-07 12:37:10 +0530

    again commint

--- Java/src/week2_OOPS/Static.java
+++ Java/src/week2_OOPS/Static.java
@@ -1,5 +1,34 @@
 package week2_OOPS;
 
 public class Static {
+	
+	static int a= 20;  // Static Variable
+	int b = 50; // Non - Static Variable
+	
+	static void data() {
+		
+		System.out.println("This is a static varible");
+	}
+	
+	void data1() {
+		
+		System.out.println("This is non - static varible");
+	}
+	
+	
+	 /* public static void main(String[] args) {
+		
+		System.out.println(a);
+		data();
+		
+		//Static st = new Static();
+		
+		System.out.println(st.b);
+		
+		st.data1();
+		
+		
+		
+	} */
 
 }
--- Java/src/week2_OOPS/StaticMain.java
+++ Java/src/week2_OOPS/StaticMain.java
@@ -4,6 +4,13 @@ public class StaticMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		System.out.println(Static.a);
+		Static.data();
+		
+		
+		
 
 	}
 
