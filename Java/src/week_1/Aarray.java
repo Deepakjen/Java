1afd0d4d5a70e2f160e0fdcbba966ddc1a2f7cd3
Deepakjen <112792076+Deepakjen@users.noreply.github.com>
2025-09-16 06:53:38 +0530

    Array commit

--- Java/src/week_1/Aarray.java
+++ Java/src/week_1/Aarray.java
@@ -3,7 +3,23 @@ package week_1;
 public class Aarray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/* int a[] = new int[5];
+		 
+		 a[0] = 10;
+		 a[1] = 20;
+		 a[2] = 30;
+		 a[3] = 40;
+		 a[4] = 50;*/
+		 
+		 
+		 int a[] = {10, 20, 30, 40, 50};
+		 
+		 
+		
+		
+		 
+		
 		
 		
 		
